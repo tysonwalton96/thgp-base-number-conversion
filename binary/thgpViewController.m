a62e203d6923983ca78594bfefd152134373495f
tyson <tyson@labs.hiddengeniusproject.org>
2012-06-28 12:38:08 -0700

    Final edited code for bases 3-9 on converter.

--- binary/thgpViewController.m
+++ binary/thgpViewController.m
@@ -70,7 +70,33 @@
  * BASE 3 Conversion
  ****/
 -(IBAction)base3:(id)sender 
-{
+{NSString *output;
+    
+    int I;
+    int Q =  [inputText.text intValue];
+    int remainder;
+    int B[20];
+    NSLog (@"Input Text = %@", inputText);
+    
+    NSLog(@"Input Number = %d", Q);
+    
+    for (I = 0; I < 20; I = I + 1) {
+        B[I] = 0;
+    }
+    
+    I = 0;
+    
+    while (Q > 0) {
+        remainder = Q % 3;
+        B[I] = remainder ;
+        Q = Q / 3; 
+        I = I + 1;
+    }
+    output = [NSString stringWithFormat:@"%d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d", B[19], B[18], B[17], B[16], B[15], B[14], B[13], B[12], B[11], B[10], B[9], B[8], B[7], B[6], B[5], B[4], B[3], B[2], B[1], B[0]];
+    NSLog(@"Binary Number =  %d%d%d%d %d%d%d%d %d%d%d%d", B[11], B[10], B[9], B[8], B[7], B[6], B[5], B[4], B[3], B[2], B[1], B[0]);
+    
+    outputText.text = output;
+
     
 }
 
@@ -78,7 +104,33 @@
  * BASE 4 Conversion
  ****/
 -(IBAction)base4:(id)sender 
-{
+{NSString *output;
+    
+    int I;
+    int Q =  [inputText.text intValue];
+    int remainder;
+    int B[20];
+    NSLog (@"Input Text = %@", inputText);
+    
+    NSLog(@"Input Number = %d", Q);
+    
+    for (I = 0; I < 20; I = I + 1) {
+        B[I] = 0;
+    }
+    
+    I = 0;
+    
+    while (Q > 0) {
+        remainder = Q % 4;
+        B[I] = remainder ;
+        Q = Q / 4; 
+        I = I + 1;
+    }
+    output = [NSString stringWithFormat:@"%d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d", B[19], B[18], B[17], B[16], B[15], B[14], B[13], B[12], B[11], B[10], B[9], B[8], B[7], B[6], B[5], B[4], B[3], B[2], B[1], B[0]];
+    NSLog(@"Binary Number =  %d%d%d%d %d%d%d%d %d%d%d%d", B[11], B[10], B[9], B[8], B[7], B[6], B[5], B[4], B[3], B[2], B[1], B[0]);
+    
+    outputText.text = output;
+
     
 }
 
@@ -86,7 +138,33 @@
  * BASE 5 Conversion
  ****/
 -(IBAction)base5:(id)sender 
-{
+{NSString *output;
+    
+    int I;
+    int Q =  [inputText.text intValue];
+    int remainder;
+    int B[20];
+    NSLog (@"Input Text = %@", inputText);
+    
+    NSLog(@"Input Number = %d", Q);
+    
+    for (I = 0; I < 20; I = I + 1) {
+        B[I] = 0;
+    }
+    
+    I = 0;
+    
+    while (Q > 0) {
+        remainder = Q % 5;
+        B[I] = remainder ;
+        Q = Q / 5; 
+        I = I + 1;
+    }
+    output = [NSString stringWithFormat:@"%d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d", B[19], B[18], B[17], B[16], B[15], B[14], B[13], B[12], B[11], B[10], B[9], B[8], B[7], B[6], B[5], B[4], B[3], B[2], B[1], B[0]];
+    NSLog(@"Binary Number =  %d%d%d%d %d%d%d%d %d%d%d%d", B[11], B[10], B[9], B[8], B[7], B[6], B[5], B[4], B[3], B[2], B[1], B[0]);
+    
+    outputText.text = output;
+
     
 }
 
@@ -94,7 +172,33 @@
  * BASE 6 Conversion
  ****/
 -(IBAction)base6:(id)sender 
-{
+{NSString *output;
+    
+    int I;
+    int Q =  [inputText.text intValue];
+    int remainder;
+    int B[20];
+    NSLog (@"Input Text = %@", inputText);
+    
+    NSLog(@"Input Number = %d", Q);
+    
+    for (I = 0; I < 20; I = I + 1) {
+        B[I] = 0;
+    }
+    
+    I = 0;
+    
+    while (Q > 0) {
+        remainder = Q % 6;
+        B[I] = remainder ;
+        Q = Q / 6; 
+        I = I + 1;
+    }
+    output = [NSString stringWithFormat:@"%d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d", B[19], B[18], B[17], B[16], B[15], B[14], B[13], B[12], B[11], B[10], B[9], B[8], B[7], B[6], B[5], B[4], B[3], B[2], B[1], B[0]];
+    NSLog(@"Binary Number =  %d%d%d%d %d%d%d%d %d%d%d%d", B[11], B[10], B[9], B[8], B[7], B[6], B[5], B[4], B[3], B[2], B[1], B[0]);
+    
+    outputText.text = output;
+
     
 }
 
@@ -102,7 +206,33 @@
  * BASE 7 Conversion
  ****/
 -(IBAction)base7:(id)sender 
-{
+{NSString *output;
+    
+    int I;
+    int Q =  [inputText.text intValue];
+    int remainder;
+    int B[20];
+    NSLog (@"Input Text = %@", inputText);
+    
+    NSLog(@"Input Number = %d", Q);
+    
+    for (I = 0; I < 20; I = I + 1) {
+        B[I] = 0;
+    }
+    
+    I = 0;
+    
+    while (Q > 0) {
+        remainder = Q % 7;
+        B[I] = remainder ;
+        Q = Q / 7; 
+        I = I + 1;
+    }
+    output = [NSString stringWithFormat:@"%d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d", B[19], B[18], B[17], B[16], B[15], B[14], B[13], B[12], B[11], B[10], B[9], B[8], B[7], B[6], B[5], B[4], B[3], B[2], B[1], B[0]];
+    NSLog(@"Binary Number =  %d%d%d%d %d%d%d%d %d%d%d%d", B[11], B[10], B[9], B[8], B[7], B[6], B[5], B[4], B[3], B[2], B[1], B[0]);
+    
+    outputText.text = output;
+
     
 }
 
@@ -110,7 +240,33 @@
  * BASE 8 Conversion
  ****/
 -(IBAction)base8:(id)sender 
-{
+{NSString *output;
+    
+    int I;
+    int Q =  [inputText.text intValue];
+    int remainder;
+    int B[20];
+    NSLog (@"Input Text = %@", inputText);
+    
+    NSLog(@"Input Number = %d", Q);
+    
+    for (I = 0; I < 20; I = I + 1) {
+        B[I] = 0;
+    }
+    
+    I = 0;
+    
+    while (Q > 0) {
+        remainder = Q % 8;
+        B[I] = remainder ;
+        Q = Q / 8; 
+        I = I + 1;
+    }
+    output = [NSString stringWithFormat:@"%d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d", B[19], B[18], B[17], B[16], B[15], B[14], B[13], B[12], B[11], B[10], B[9], B[8], B[7], B[6], B[5], B[4], B[3], B[2], B[1], B[0]];
+    NSLog(@"Binary Number =  %d%d%d%d %d%d%d%d %d%d%d%d", B[11], B[10], B[9], B[8], B[7], B[6], B[5], B[4], B[3], B[2], B[1], B[0]);
+    
+    outputText.text = output;
+
     
 }
 
@@ -118,7 +274,33 @@
  * BASE 9 Conversion
  ****/
 -(IBAction)base9:(id)sender 
-{
+{NSString *output;
+    
+    int I;
+    int Q =  [inputText.text intValue];
+    int remainder;
+    int B[20];
+    NSLog (@"Input Text = %@", inputText);
+    
+    NSLog(@"Input Number = %d", Q);
+    
+    for (I = 0; I < 20; I = I + 1) {
+        B[I] = 0;
+    }
+    
+    I = 0;
+    
+    while (Q > 0) {
+        remainder = Q % 9;
+        B[I] = remainder ;
+        Q = Q / 9; 
+        I = I + 1;
+    }
+    output = [NSString stringWithFormat:@"%d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d", B[19], B[18], B[17], B[16], B[15], B[14], B[13], B[12], B[11], B[10], B[9], B[8], B[7], B[6], B[5], B[4], B[3], B[2], B[1], B[0]];
+    NSLog(@"Binary Number =  %d%d%d%d %d%d%d%d %d%d%d%d", B[11], B[10], B[9], B[8], B[7], B[6], B[5], B[4], B[3], B[2], B[1], B[0]);
+    
+    outputText.text = output;
+
     
 }
 

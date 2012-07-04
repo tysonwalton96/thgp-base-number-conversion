973634a8258bc3df2fc1ec308f45b33a70df71e9
tyson <tyson@labs.hiddengeniusproject.org>
2012-07-03 17:27:28 -0700

    attached convert button

--- binary/thgpViewController.h
+++ binary/thgpViewController.h
@@ -13,18 +13,12 @@
     UITextField *inputText;
     UITextField *outputText;
     
+    
 }
 
 @property (nonatomic, retain) IBOutlet UITextField *inputText;
 @property (nonatomic, retain) IBOutlet UITextField *outputText;
+-(IBAction)converter:(id)sender;
 
--(IBAction)base2:(id)sender;
--(IBAction)base3:(id)sender;
--(IBAction)base4:(id)sender;
--(IBAction)base5:(id)sender;
--(IBAction)base6:(id)sender;
--(IBAction)base7:(id)sender;
--(IBAction)base8:(id)sender;
--(IBAction)base9:(id)sender;
 
 @end
